928a1d9bf82ac2d1822e42bb5e281b1b92425888
ArtZ <snickgem@gmail.com>
2022-06-17 19:10:24 +0400

    fix

--- src/js/partials/swiper-init.js
+++ src/js/partials/swiper-init.js
@@ -6,6 +6,7 @@ $(window).on('load', function () {
 			var slider = new Swiper(elem, {
 				slidesPerView: 1,
 				spaceBetween: 5,
+				speed: 500,
 				loop: true,
 				autoplay: {
 					delay: 3000,
@@ -30,16 +31,16 @@ $(window).on('load', function () {
 					slideChange: function() {
 						switch (this.realIndex) {
 							case 0:
-								$('.first-screen__title span').text('Lavazza LB 1100');
+								$('.first-screen__title .model').text('LB 1100');
 								break;
 							case 1:
-								$('.first-screen__title span').text('Lavazza Lep Matinee');
+								$('.first-screen__title .model').text('Lep Matinee');
 								break;
 							case 2:
-								$('.first-screen__title span').text('Lavazza LB 1100');
+								$('.first-screen__title .model').text('LB 1100');
 								break;
 							case 3:
-								$('.first-screen__title span').text('Lavazza Blue 951');
+								$('.first-screen__title .model').text('Blue 951');
 								break;
 						}
 					},
